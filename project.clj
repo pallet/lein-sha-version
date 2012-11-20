a3614309b6984d62f21039f78eac7630150f802f
Hugo Duncan <hugo@hugoduncan.org>
2012-11-20 16:36:49 -0500

    Fix project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-sha-version "0.1.0-SNAPSHOT"
+(defproject lein-sha-version "0.1.1-SNAPSHOT"
   :description "A Leiningen plugin to set the project version based on the git SHA."
   :url "https://github.com/pallet/lein-sha-version"
   :license {:name "Eclipse Public License"
