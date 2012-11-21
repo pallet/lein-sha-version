87547256de6633b53f9c3769d42dd9448e1d5dc8
Hugo Duncan <hugo@hugoduncan.org>
2012-11-20 19:48:32 -0500

    Use l.c.main/debug for output

--- src/lein_sha_version/plugin.clj
+++ src/lein_sha_version/plugin.clj
@@ -1,12 +1,13 @@
 (ns lein-sha-version.plugin
   (:use
-   [clojure.java.io :only [file]])
+   [clojure.java.io :only [file]]
+   [leiningen.core.main :only [debug]])
   (:import
    org.eclipse.jgit.storage.file.FileRepositoryBuilder
    [org.eclipse.jgit.lib ObjectId Repository]))
 
 (defn git-sha [{:keys [root version sha]}]
-  (println "Finding SHA for" root)
+  (debug "Finding SHA for" root)
   (let [^Repository repository (.. (FileRepositoryBuilder.)
                                    (readEnvironment)
                                    (findGitDir (file root))
@@ -14,6 +15,7 @@
         ^ObjectId head (.resolve repository "HEAD")]
     (if head
       (let [abbr (.abbreviate head (:length sha 7))]
+        (debug "Found SHA" (.name head) "using" (.name abbr))
         (.name abbr))
       version)))
 
