b116122a20ad1261709334882e3e9a96f1a64554
Stephen Starkey <scstarkey@gmail.com>
2015-12-09 16:35:36 -0600

    Added some error handling.

--- src/lein_sha_version/plugin.clj
+++ src/lein_sha_version/plugin.clj
@@ -1,22 +1,26 @@
 (ns lein-sha-version.plugin
   (:use
     [clojure.java.io :only [file]]
-    [leiningen.core.main :only [debug]])
+    [leiningen.core.main :only [warn debug]])
   (:import
     org.eclipse.jgit.storage.file.FileRepositoryBuilder
     [org.eclipse.jgit.lib ObjectId Repository]))
 
 (defn git-sha [{:keys [root version sha]}]
   (debug "Finding SHA for" root)
-  (let [^Repository repository (.. (FileRepositoryBuilder.)
-                                   (readEnvironment)
-                                   (findGitDir (file root))
-                                   (build))
-        ^ObjectId head (.resolve repository "HEAD")]
-    (if head
-      (let [abbr (.abbreviate head (:length sha 7))]
-        (debug "Found SHA" (.name head) "using" (.name abbr))
-        (.name abbr))
+  (try
+    (let [^Repository repository (.. (FileRepositoryBuilder.)
+                                     (readEnvironment)
+                                     (findGitDir (file root))
+                                     (build))
+          ^ObjectId head (.resolve repository "HEAD")]
+      (if head
+        (let [abbr (.abbreviate head (:length sha 7))]
+          (debug "Found SHA" (.name head) "using" (.name abbr))
+          (.name abbr))
+        version))
+    (catch Throwable e
+      (warn (format "Unable to get SHA: %s" (.getMessage e)))
       version)))
 
 (defn update-manifest [git-sha {manifest                  :manifest
@@ -27,8 +31,8 @@
 
 (defn middleware
   [project]
-  (let [sha (git-sha project)
-        manifest (update-manifest sha project)]
+  (let [version (git-sha project)
+        manifest (update-manifest version project)]
     (-> project
-        (assoc :version sha)
+        (assoc :version version)
         (assoc :manifest manifest))))
