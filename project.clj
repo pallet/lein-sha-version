e16477d9662b22ec27258266b766bd565f7851b6
Stephen Starkey <scstarkey@gmail.com>
2015-12-09 15:46:45 -0600

    Put project name back.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.centriqhome.leiningen/lein-sha-version "0.2.0-SNAPSHOT"
+(defproject lein-sha-version "0.2.0-SNAPSHOT"
   :description "A Leiningen plugin to set the project version based on the git SHA."
   :url "https://github.com/pallet/lein-sha-version"
   :license {:name "Eclipse Public License"
